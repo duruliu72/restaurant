8fe852dc9faeeed30954abc67ed3edde3009ffd1
duruliu72 <duruliu72@gmail.com>
2018-06-07 19:56:39 +0600

    2nd commit 07 06 3018

--- js/script.js
+++ js/script.js
@@ -1,38 +1,72 @@
-$(document).ready(function () {
-    var scrolllink = $('.scroll');
+ $(document).ready(function () {
+     $('#main-slider').owlCarousel({
+         loop: true,
+         margin: 10,
+         nav: false,
+         margin: 10,
+         items: 1
+     });
+     $('#sp-offer').owlCarousel({
+         loop: true,
+         autoplay: true,
+         margin: 10,
+         responsive: {
+             // breakpoint from 0 up
+             0: {
+                 items: 1
+             },
+             // breakpoint from 480 up
+             480: {
+                 items: 2,
+             },
+             // breakpoint from 768 up
+             768: {
+                 items: 4,
+             }
+         }
+     });
+     var containerEl = document.querySelector('.grid');
+     var mixer = mixitup(containerEl);
+     $('.cheaf-carousel').owlCarousel({
+         loop: true,
+         autoplay: true,
+         margin: 15,
+         responsive: {
+             // breakpoint from 0 up
+             0: {
+                 items: 1
+             },
+             // breakpoint from 480 up
+             480: {
+                 items: 2,
+             },
+             // breakpoint from 768 up
+             768: {
+                 items: 4,
+             }
+         }
+     });
 
-    //    Smoth scrolling
-    scrolllink.click(function (e) {
-        e.preventDefault();
-        $('body,html').animate({
-            scrollTop: $(this.hash).offset().top
-        }, 1000);
-    });
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.scroll').click(function () {
-        $('.navbar-collapse').collapse('hide');
-    });
-    //    Active link switching
-    $(window).scroll(function () {
-        var scrollbarLocation = $(this).scrollTop();
-        scrolllink.each(function () {
-            var sectionOffset = $(this.hash).offset().top;
-            if (sectionOffset <= scrollbarLocation) {
-                console.log(this);
-                $(this).parent().addClass('active');
-                $(this).parent().siblings().removeClass('active');
-            }
-        });
-    });
-});
-//fixed menu
-$(function () {
-    var headerbottom = document.getElementById('bg');
-    window.onscroll = function () {
-        if (window.pageYOffset <= 513) {
-            headerbottom.style.background = "rgba(0,0,0,0.0)";
-        } else {
-            headerbottom.style.background = "#000";
-        }
-    }
-});
+     //    For scrolling
+     var scrolllink = $('.scroll');
+     //    smooth scrolling
+     scrolllink.click(function (e) {
+         e.preventDefault();
+         $('body,html').animate({
+             scrollTop: $(this.hash).offset().top - 50,
+         }, 1000);
+     });
+     //    Active link switching
+     $(window).scroll(function () {
+         var scrollbarlocation = $(this).scrollTop() + 65;
+         scrolllink.each(function () {
+             var sectionoffset = $(this.hash).offset().top;
+             if (sectionoffset <= scrollbarlocation) {
+                 $(this).parent().addClass('active');
+                 $(this).parent().siblings().removeClass('active');
+             }
+         });
+     });
+     //AOS Effect
+     AOS.init();
+ });
